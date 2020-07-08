87655de5c75f2d558d913cf866c6093db5bd73c3
Mark <markandrewhamm@gmail.com>
2020-07-08 11:55:22 -0500

    index and show action

--- app/controllers/api/balls_controller.rb
+++ app/controllers/api/balls_controller.rb
@@ -1,2 +1,23 @@
 class Api::BallsController < ApplicationController
+  def index
+    @balls = Ball.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @ball = Ball.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  # def create
+    
+  # end
+
+  # def update
+    
+  # end
+
+  # def destroy
+
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/balls" => "balls#index"
+    get "/balls/:id" => "balls#show"
+    post "/balls" => "balls#create"
+    patch "/balls/:id" => "balls#update"
+    delete "/balls/:id" => "balls#destroy"
+  end
 end
